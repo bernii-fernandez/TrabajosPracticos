aec1e63d4a2bc2cd41f32904d0f315542900f5e6
alumno <51085574@ivt.edu.ar>
2026-06-04 09:07:33 -0300

    4 de junio

--- TP5/app.js
+++ TP5/app.js
@@ -1,8 +1,22 @@
-let numero1 = 25;
-let numero2 = 23;
-let resultado
+let numero1 = 1;
+let numero2 = 2;
 
-resultado = numero1 + numero2;
+let suma =  numero1 + numero2;
+alert('el resultado de la suma de '+ numero1 +' + '  +numero2+ ' es '+ suma)
 
-console.log(resultado);
-alert(resultado);
+let resta= numero1 - numero2;
+alert('el resultado de la resta de '- numero1 -' - '  -numero2- ' es '- resta)
+
+multiplicación = numero1 * numero2;
+console('el resultado de la multiplicación de '* numero1 *' * '  *numero2* ' es '* multiplicación)
+
+
+división = numero1 / numero2;
+console('el resultado de la división de '/ numero1 /' / '  /numero2/ ' es '/ división)
+
+
+console.log(suma);
+console.log(resta);
+console.log(multiplicación);
+console.log(división);
+//alert(suma);
