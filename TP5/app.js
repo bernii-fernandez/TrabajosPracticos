8d4ec59f1b382988ff177cea9a094c81ff89bb08
alumno <alumno@Instituto.local>
2026-06-11 08:20:55 -0300

    Update app.js

--- TP5/app.js
+++ TP5/app.js
@@ -20,3 +20,32 @@ console.log(resta);
 console.log(multiplicación);
 console.log(división);
 //alert(suma);
+
+let nombreAlumno = simo;
+let apellidoAlumno = echeveste;
+let edadAlumno = 14;
+console( nombreAlumno +' '+ apellidoAlumno +' tiene '+ edadAlumno );
+
+let precio1= 3;
+let precio2= 5;
+let precio3= 10;
+let multiplicacion= 2;
+let division= 3;
+
+log("el resultado de la division es:" + division);
+log("el resultado de la multiplicacion es:" + multiplicacion)
+log("resultado de la suma es:" + suma);
+log("resultado de la suma es:" + resta);
+
+let total1=  precio1 + precio2 + precio3;
+console(' el total a pagar es: ' +precio1+ ' + '  +precio2+ ' + ' +precio3+ ' es ' +total1)
+
+let comidafavorita= tacos
+let colorFavorito= verde
+let numeroFavorito= 1
+console.log("Mi comida favorita son los tacos, me gusta el color verde y mi número favorito es el 1")
+
+let pais= 'Argentina'
+console(pais)
+pais='Brasil'
+console(pais)
