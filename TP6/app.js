b71edb40c1a8807ec27dfb090726d9691eed3922
alumno <alumno@Instituto.local>
2026-07-02 08:17:50 -0300

    2/7

--- TP6/app.js
+++ TP6/app.js
@@ -2,11 +2,13 @@ let uno = document.querySelector('#b1')
 let dos = document.querySelector('#b2')
 let tres = document.querySelector('#b3')
 let cuatro = document.querySelector('#b4')
+let cinco = document.querySelector('#b5')
+let seis = document.querySelector('#b6')
 let p= document.querySelector('p')
 
 uno.onclick= function(){
     //cambiar el color de fondo
-    p.style.backgroundColor= 'white'
+    p.style.backgroundColor= 'rgb(145, 189, 255)'
 }
 dos.onclick= function(){
     //cambiar el contenido del parrafo
@@ -19,4 +21,17 @@ tres.onclick= function(){
 cuatro.onclick= function(){
     //cambiar el tipo de letra del párrafo
     p.style.fontFamily= 'IMPACT'
+}
+cinco.onclick= function(){
+    //cambiar el tamaño del párrafo
+    p.style.fontSize= '50px'
+}
+
+seis.onclick= function(){
+    //reiniciar
+    p.style.fontSize= '25px'
+    p.style.fontFamily= 'arial'
+    p.style.backgroundColor= 'rgb(218, 68, 130)'
+    p.style.color= 'rgb(255, 255, 255)'
+    p.style.textContent= 'Este parrafo puede modificarse'
 }
